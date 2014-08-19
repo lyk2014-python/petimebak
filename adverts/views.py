0ad961edb23190b9a765804fdcef3c05b9696cd1
fatiherikli <fatih@hipo.biz>
2014-08-19 16:28:23 +0300

    Exclude non-published adverts

--- adverts/views.py
+++ adverts/views.py
@@ -6,7 +6,9 @@ from adverts.models import Advert
 
 def home(request):
 
-    adverts = Advert.objects.all()
+    adverts = (Advert.objects
+                .filter(is_published=True)
+                .order_by("-date_created"))
 
     return render_to_response("index.html", {
         "adverts": adverts
