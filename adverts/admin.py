34e93e59134e4068bdbaa0cbd7c3bcaa99cb0f16
fatiherikli <fatih@hipo.biz>
2014-08-19 15:04:03 +0300

    Initial Commit

---
+++ adverts/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from adverts.models import Advert, PetType
+
+
+admin.site.register(Advert)
+admin.site.register(PetType)
---
+++ adverts/models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.encoding import smart_unicode
+
+
+class PetType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
+
+    class Meta:
+        verbose_name = "Pet Tipi"
+        verbose_name_plural = "Pet Tipleri"
+
+class Advert(models.Model):
+    region = models.CharField(max_length=255)
+    pet_type = models.ForeignKey("PetType")
+    user = models.ForeignKey(User)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return smart_unicode("%s -> %s " %(self.user.username, self.pet_type))
+
+    class Meta:
+        verbose_name = "İlan"
+        verbose_name_plural = "İlanlar"
