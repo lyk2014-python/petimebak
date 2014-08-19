2e807ac327ab61e454fd0683fcae142c97e4a81d
fatiherikli <fatih@hipo.biz>
2014-08-19 16:04:04 +0300

    Show adverts in homepage

--- adverts/views.py
+++ adverts/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from adverts.models import Advert
+
 
 def home(request):
-    return render_to_response("index.html")
+
+    adverts = Advert.objects.all()
+
+    return render_to_response("index.html", {
+        "adverts": adverts
+    }, RequestContext(request))
