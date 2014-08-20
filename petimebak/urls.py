c6d763ea7f33d554a6812461df81bd564307d7f3
fatiherikli <fatih@hipo.biz>
2014-08-20 11:10:45 +0300

    Implement login/logout views

--- petimebak/urls.py
+++ petimebak/urls.py
@@ -6,7 +6,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'adverts.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^login$', 'profiles.views.login', name='login'),
+    url(r'^logout', 'profiles.views.logout', name='logout'),
 
+    # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.contrib.auth import login as auth_login, logout as auth_logout
 
-# Create your views here.
+from profiles.forms import LoginForm
+
+
+def login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(username=username,
+                                password=password)
+
+            auth_login(request, user)
+
+            return redirect(reverse("home"))
+
+
+    return render_to_response("login.html", {
+        "form": form
+    }, RequestContext(request))
+
+
+def logout(request):
+    auth_logout(request)
+    return render_to_response("logout.html", {},
+                              RequestContext(request))
