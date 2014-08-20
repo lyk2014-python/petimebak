ea59c02441dd129715d51220b24cf509930356f1
fatiherikli <fatih@hipo.biz>
2014-08-20 12:28:01 +0300

    Login automatically after registration

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
-from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 
 from profiles.forms import LoginForm, RegistrationForm
 
@@ -32,6 +32,11 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username,
+                                password=password)
+            auth_login(request, user)
             return redirect(reverse("home"))
 
     return render_to_response("register.html", {
