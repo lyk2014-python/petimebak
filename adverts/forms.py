94f76b59b84cb230ee8778877ed53358ba054d54
Halit Alptekin <info@halitalptekin.com>
2014-08-21 11:28:08 +0300

    Add captcha

--- adverts/forms.py
+++ adverts/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from adverts.models import Advert
-
+from captcha.fields import CaptchaField
 
 class AdvertCreationForm(forms.ModelForm):
+    captcha = CaptchaField()
     class Meta:
         fields = ['pet_type', 'start_date',
                   'end_date', 'region', 'price']
