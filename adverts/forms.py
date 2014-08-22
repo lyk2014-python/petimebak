e8b89ba8673a2dffab67492876e7bd3c269e97b6
Omer Oguz <omerouz@gmail.com>
2014-08-22 19:47:17 +0300

    end/startdate error fixed

--- adverts/forms.py
+++ adverts/forms.py
@@ -1,15 +1,22 @@
+# -*- coding: utf-8 -*-
+
 from django import forms
-from adverts.models import Advert, AdvertImage
+from django.core.exceptions import ValidationError
+from adverts.models import Advert
+
 
 class AdvertCreationForm(forms.ModelForm):
 
     class Meta:
-        fields = ['pet_type', 'start_date',
-                  'end_date', 'region', 'price']
+
+        fields = ['pet_type', 'start_date', 'end_date', 'region', 'price']
+
         model = Advert
 
 
-class PhotoCreationForm(forms.ModelForm):
-    class Meta:
-        fields = ['title', 'image']
-        model = AdvertImage
+    def cleandate(self):
+        start_date = self.cleaned_data.get("start_date")
+        end_date = self.cleaned_data.get("end_date")
+
+        if end_date < start_date:
+            raise ValidationError("Bitiş tarihi başlangıç tarihinden erken olamaz!")
