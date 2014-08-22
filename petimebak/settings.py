6bf97cc9e598be838c43c7447cc75ec1bbe5e217
fatiherikli <fatih@hipo.biz>
2014-08-22 15:43:47 +0300

    Configure database url

--- petimebak/settings.py
+++ petimebak/settings.py
@@ -79,7 +79,9 @@ WSGI_APPLICATION = 'petimebak.wsgi.application'
 #}
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    "default": dj_database_url.config(default='postgres://localhost')
+}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
