6b1f7a9f90bed01db2d0dcfc7d4fcff4d6caa4a6
fatiherikli <fatih@hipo.biz>
2014-08-22 15:38:52 +0300

    Configure heroku settings

--- petimebak/settings.py
+++ petimebak/settings.py
@@ -71,12 +71,17 @@ WSGI_APPLICATION = 'petimebak.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
--- petimebak/wsgi.py
+++ petimebak/wsgi.py
@@ -11,4 +11,10 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "petimebak.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
+
+
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
