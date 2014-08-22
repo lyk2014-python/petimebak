fdf128b95adc0adb01a31cdfd7abbc4d81646a21
fatiherikli <fatih@hipo.biz>
2014-08-22 17:18:39 +0300

    Add email field in registration

--- profiles/forms.py
+++ profiles/forms.py
@@ -3,6 +3,7 @@
 from django import forms
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 
@@ -29,4 +30,8 @@ class LoginForm(forms.Form):
 
 
 class RegistrationForm(UserCreationForm):
-    pass
+
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
