288557f720f300e3e20d2cbabe20bd0458400d27
Halit Alptekin <info@halitalptekin.com>
2014-08-24 16:54:37 +0300

    Add built-in django min value validator

--- adverts/models.py
+++ adverts/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.encoding import smart_unicode
-from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 
 
 class PetType(models.Model):
@@ -16,24 +16,21 @@ class PetType(models.Model):
         verbose_name_plural = "Pet Tipleri"
 
 
-def validate_positive(value):
-    if value < 0.01:
-        raise ValidationError(u'pozitif değer girin')
-
-
 class Advert(models.Model):
     region = models.CharField(max_length=255)
     pet_type = models.ForeignKey("PetType")
     user = models.ForeignKey(User)
     start_date = models.DateField()
     end_date = models.DateField()
-    price = models.DecimalField(decimal_places=2, max_digits=5, validators=[validate_positive])
+    price = models.DecimalField(decimal_places=2, max_digits=5,
+                                validators=[MinValueValidator(0)])
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
 
     def __unicode__(self):
-        return smart_unicode("%s -> %s " %(self.user.username, self.pet_type.name))
+        return smart_unicode("%s -> %s " % (self.user.username,
+                                            self.pet_type.name))
 
     class Meta:
         verbose_name = "İlan"
